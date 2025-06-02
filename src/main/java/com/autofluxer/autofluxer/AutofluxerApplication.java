d1deed70fc861ff1e337e33738b29764b7d21698
Pedro Italo <pedroitl15@gmail.com>
2025-06-02 20:05:31 -0300

    Updated aplication

--- src/main/java/com/autofluxer/autofluxer/AutofluxerApplication.java
+++ src/main/java/com/autofluxer/autofluxer/AutofluxerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutofluxerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AutofluxerApplication.class, args);
 	}
 
